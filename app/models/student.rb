edce861d6bf35fba05db4e72e63c779025fa907c
Brian Dorton <dorton@gmail.com>
2016-07-07 13:53:24 -0500

    fixes show, validate p of all the things

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,6 @@ class Student < ActiveRecord::Base
   has_many :studentcohorts
   has_many :cohorts, through: :studentcohorts, dependent: :destroy
   attachment :profile_image
+  validates_presence_of :name, :fun_fact, :cohorts, :profile_image
 
 end
