e73bd937673eeb323313c8313453cbc7ee7af8ea
Brian Dorton <dorton@gmail.com>
2016-07-07 12:58:22 -0500

    fixes the old refile-postgres gem again

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,13 +3,4 @@ class Student < ActiveRecord::Base
   has_many :cohorts, through: :studentcohorts, dependent: :destroy
   attachment :profile_image
 
-  def upload=(value)
-    write_attribute(:profile_image, value.read) if value.respond_to? :read
-  end
-
-  def self.quick_all
-    exclude_big_columns = ['upload']
-    columns = Student.attribute_names - exclude_big_columns
-    all.select(columns)
-  end
 end
